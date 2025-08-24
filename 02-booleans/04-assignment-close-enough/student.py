cc4826217313f956778494a11c66fd6ac5586a05
Daphne Zadora <daphne.zadora@student.ucll.be>
2025-08-24 15:52:47 +0200

    finished booleans assignments

--- 02-booleans/04-assignment-close-enough/student.py
+++ 02-booleans/04-assignment-close-enough/student.py
@@ -1,1 +1,2 @@
-# write your code here
+def close_enough(x, y):
+    return abs(x -y) <= 0.1
--- 02-booleans/06-assignment-dates/student.py
+++ 02-booleans/06-assignment-dates/student.py
@@ -1,1 +1,43 @@
-# write your code here
+def is_valid_month(month):
+    return 1 <= month <= 12
+
+
+def is_leap_year(year):
+    if year % 400 == 0:
+        return True
+    if year % 100 == 0:
+        return False
+    if year % 4 == 0:
+        return True
+    return False
+
+
+def has_30_days(month):
+    return month in (4, 6, 9, 11)
+
+
+def has_31_days(month):
+    return month in (1, 3, 5, 7, 8, 10, 12)
+
+
+def has_28_days(month, year):
+    return month == 2 and not is_leap_year(year)
+
+
+def has_29_days(month, year):
+    return month == 2 and is_leap_year(year)
+
+
+def is_valid_date(day, month, year):
+    if not is_valid_month(month):
+        return False
+    if has_30_days(month):
+        return 1 <= day <= 30
+    if has_31_days(month):
+        return 1 <= day <= 31
+    if has_28_days(month, year):
+        return 1 <= day <= 28
+    if has_29_days(month, year):
+        return 1 <= day <= 29
+    else:
+        return False
