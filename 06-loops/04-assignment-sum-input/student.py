4b650feeebbd125368e249aa47fc1e0f368c6dd3
Daphne Zadora <daphne.zadora@student.ucll.be>
2025-08-27 19:06:22 +0200

    Update

--- 06-loops/04-assignment-sum-input/student.py
+++ 06-loops/04-assignment-sum-input/student.py
@@ -1,1 +1,7 @@
-# write your code here
+def sum_input():
+    total = 0
+    number = int(input("Enter integer: "))
+    while number != 0:
+        total += number
+        number = int(input("Enter integer: "))
+    print(f"The sum equals {total}.")
